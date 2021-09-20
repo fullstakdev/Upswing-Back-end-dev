239a6bebfc2d1405dc568c42d6302d9faa0ae37b
william <william.james@nearix.com>
2021-09-20 14:11:39 -0400

    latest

--- functions/src/middleware/auth.ts
+++ functions/src/middleware/auth.ts
@@ -1,14 +1,17 @@
 import * as admin from "firebase-admin";
 import { NextFunction, Request, Response } from "express";
 import { handleError } from "../utils";
+import userRepository from "../repositories/user.repo";
 
 const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
     try {
         if (req.headers.authorization &&
             req.headers.authorization.startsWith("Bearer ")
         ) {
-            const token = req.headers.authorization;
-            req.body.user = await admin.auth().verifyIdToken(token);
+            const token = req.headers.authorization.split('Bearer ')[1];
+            const decodedData = await admin.auth().verifyIdToken(token);
+            const user = await userRepository.getUserById(decodedData.uid);
+            req.body.user = user;
             return next();
         }
         handleError(res, { code: 403, message: "Unauthenticated" });
--- functions/src/repositories/user.repo.ts
+++ functions/src/repositories/user.repo.ts
@@ -1,3 +1,6 @@
+import { db } from "../.";
+import { COLLECTION_USER } from "../utils/constants";
+
 const createUser = () => {};
 
 const updateUser = () => {};
@@ -8,10 +11,15 @@ const getUser = () => {};
 
 const deleteUser = () => {};
 
+const getUserById = async (id: string) => {
+    return await (await db.collection(COLLECTION_USER).where('id', '==', id).get()).docs[0].data();
+};
+
 export default {
     createUser,
     updateUser,
     getUsers,
     getUser,
+    getUserById,
     deleteUser,
 };

35a189d7aefadfdb2fe341c8549d891e1889f05f
daily-dev87 <tree87820@gmail.com>
2021-09-24 17:35:52 +0300

    add search function in exercise

--- functions/src/repositories/exercise.repo.ts
+++ functions/src/repositories/exercise.repo.ts
@@ -1,17 +1,30 @@
-const createProgram = () => {};
+import { db } from '../.';
+import { COLLECTION_EXERCISE } from '../utils/constants';
 
-const updateProgram = () => {};
+interface ISearchExerciseParams {
+    name?: string;
+    ids?: string[];
+    trainerId?: string;
+    page: number;
+    perPage?: number;
+}
 
-const getPrograms = () => {};
-
-const getProgram = () => {};
-
-const deleteProgram = () => {};
+const searchExercise = async (data: any) => {
+    const params: ISearchExerciseParams = data;
+    const programs = await db.collection(COLLECTION_EXERCISE).get();    
+    let resultData: any = [];
+    programs.docs.map(doc => {
+        let docData = doc.data();
+        if(params.ids && params.ids.length > 0 && !params.ids.includes(doc.id)) return;
+        if(params.name && !docData.name.includes(params.name)) return;
+        if(params.trainerId && docData.trainerId !== params.trainerId) return;
+        docData['id'] = doc.id;
+        resultData.push(docData);
+        return resultData;
+    })
+    return resultData;
+};
 
 export default {
-    createProgram,
-    updateProgram,
-    getPrograms,
-    getProgram,
-    deleteProgram,
+    searchExercise,
 };

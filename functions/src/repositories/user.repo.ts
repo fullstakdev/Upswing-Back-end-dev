2b9865ff0882b66f49ffa6e49e3b05588bfecffd
daily-dev87 <tree87820@gmail.com>
2021-09-27 20:51:52 +0300

    change user.repo

--- functions/src/repositories/user.repo.ts
+++ functions/src/repositories/user.repo.ts
@@ -5,7 +5,7 @@ interface ISearchUserParams {
     name?: string;
     gender?: string;
     email?: string;
-    userType?: string;
+    role?: string;
     goalArchive?: boolean;
 }
 
@@ -18,7 +18,7 @@ const searchUser = async (data: any) => {
         if (params.name && !docData.name.includes(params.name)) return;
         if (params.gender && docData.gender !== params.gender) return;
         if (params.email && docData.email !== params.email) return;
-        if (params.userType && docData.userType !== params.userType) return;
+        if (params.role && docData.role !== params.role) return;
         docData['id'] = doc.id;
         resultData.push(docData);
         return resultData;
